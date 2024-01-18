5c5551d10e70edacbc01d5b0be16c9594afdbb05
nichwch <nichwch@gmail.com>
2024-01-17 19:24:08 -0500

    exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,16 @@
-import BaseSelector from '../BaseSelectorPopup.svelte';
+import BaseSelectorPopup from '../BaseSelectorPopup.svelte';
+import BaseLabelAndPopup from '../BaseLabelAndPopup.svelte';
+import LabelAndPopup from '../LabelAndPopup.svelte';
+import MultiSelector from '../MultiSelector.svelte';
+import MultiSelectorPopup from '../MultiSelectorPopup.svelte';
+import SingleSelectorPopup from '../SingleSelectorPopup.svelte';
 // Reexport your entry components here
 
-export { BaseSelector };
+export {
+	BaseSelectorPopup,
+	BaseLabelAndPopup,
+	LabelAndPopup,
+	MultiSelector,
+	MultiSelectorPopup,
+	SingleSelectorPopup
+};
